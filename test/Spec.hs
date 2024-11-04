d95c0cc0b75275833df2ca02219cacb11edf60cd
Martynas Klimas <martynas.klimas@mif.stud.vu.lt>
2024-11-04 07:32:18 +0000

    Added harder tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -31,8 +31,11 @@ unitTests = testGroup "Lib1 tests"
     testCase "Parsing correct AddGuitar command" $
       isRight (Lib2.parseQuery "AddGuitar(1,Fender,200,2,Electric,none)") @?= True,
     testCase "Parsing incorrect AddAmplifier command (should be none)" $
-      isLeft (Lib2.parseQuery "AddAmplifier(2,Marshal,300,1,Tube,n)") @?= True
+      isLeft (Lib2.parseQuery "AddAmplifier(2,Marshal,300,1,Tube,n)") @?= True,
 
-      let newState = Lib2.emptyState
-      
-  ]
+   testCase "Parsing a harder variant" $
+      isRight (Lib2.parseQuery "AddAmplifier(2,Marshall,10,500,Tube,Amplifier(5,Marshall,10,300,other,none))") @?= True,
+
+   testCase "Parsing an even harder variant" $
+      isRight (Lib2.parseQuery "AddAccessory(4,HardPick,50,5,Plastic,Accessory(5,SoftPick,25,2,Plastic,Accessory(6,MediumPick,10,2,other,none)))") @?= True   
+   ]
